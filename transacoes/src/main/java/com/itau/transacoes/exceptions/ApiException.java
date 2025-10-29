ae692ad84cef2d07a221095f1f12198ef098f9a2
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-10-29 18:11:02 -0300

    Add ApiException

--- transacoes/src/main/java/com/itau/transacoes/exceptions/ApiException.java
+++ transacoes/src/main/java/com/itau/transacoes/exceptions/ApiException.java
@@ -1,4 +1,7 @@
 package com.itau.transacoes.exceptions;
 
-public class ApiException {
+public class ApiException extends RuntimeException {
+    public ApiException(String message) {
+        super(message);
+    }
 }
