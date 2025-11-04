5f9b1447d2f21d95053cba1b742d04f60eb8d523
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-11-04 15:45:11 -0300

    Adjustments

--- transacoes/src/main/java/com/itau/transacoes/controller/TransacaoController.java
+++ transacoes/src/main/java/com/itau/transacoes/controller/TransacaoController.java
@@ -1,4 +1,17 @@
 package com.itau.transacoes.controller;
 
+import com.itau.transacoes.dto.EstatisticaDTO;
+import com.itau.transacoes.dto.TransacaoDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transacao")
+@RequiredArgsConstructor
 public class TransacaoController {
+    private final EstatisticaDTO estatisticaDTO;
+    private final TransacaoDTO transacaoDTO;
+
+
 }
