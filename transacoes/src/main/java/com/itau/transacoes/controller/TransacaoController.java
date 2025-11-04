cefb90f454611c1541d22be9378119d8a41989c4
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-11-04 16:45:07 -0300

    Adjustments

--- transacoes/src/main/java/com/itau/transacoes/controller/TransacaoController.java
+++ transacoes/src/main/java/com/itau/transacoes/controller/TransacaoController.java
@@ -2,11 +2,14 @@ package com.itau.transacoes.controller;
 
 import com.itau.transacoes.dto.EstatisticaDTO;
 import com.itau.transacoes.dto.TransacaoDTO;
+import com.itau.transacoes.infrastructure.entities.Transacao;
 import com.itau.transacoes.service.EstatisticaService;
 import com.itau.transacoes.service.TransacaoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/transacao")
@@ -15,5 +18,22 @@ public class TransacaoController {
     private final EstatisticaService estatisticaService;
     private final TransacaoService transacaoService;
 
+    @PostMapping
+    public ResponseEntity<Void> adicionarTransacao(@RequestBody TransacaoDTO transacaoDTO){
+        return transacaoService.adicionarTransacao(transacaoDTO);
+    }
+
+    @GetMapping("/estatistica")
+    public ResponseEntity<EstatisticaDTO> obterEstatisticas(){
+        List<Transacao> transacoes = transacaoService.listarTransacoes();
+        EstatisticaDTO estatisticas = estatisticaService.calcularEstatisticas(transacoes);
+        return ResponseEntity.ok(estatisticas);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> limparTransacoes(){
+        transacaoService.limparTransacoes();
+        return ResponseEntity.ok().build();
+    }
 
 }
--- transacoes/src/main/java/com/itau/transacoes/dto/TransacaoDTO.java
+++ transacoes/src/main/java/com/itau/transacoes/dto/TransacaoDTO.java
@@ -1,4 +1,15 @@
 package com.itau.transacoes.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransacaoDTO {
+    private Double valor;
+    private OffsetDateTime dataHora;
 }
