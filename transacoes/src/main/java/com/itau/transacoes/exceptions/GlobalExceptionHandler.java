963b603585d60c309cc1862cafab3ea3dd74d272
Daniel-Macedo-dev <danielmacedosilva11@gmail.com>
2025-11-04 15:50:52 -0300

    Exceptions

--- transacoes/src/main/java/com/itau/transacoes/exceptions/GlobalExceptionHandler.java
+++ transacoes/src/main/java/com/itau/transacoes/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,19 @@
 package com.itau.transacoes.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(TransacaoInvalidaException.class)
+    public ResponseEntity<Void> handleTransacaoInvalida(TransacaoInvalidaException exception){
+        return ResponseEntity.unprocessableEntity().build();
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Void> handleGeneric(Exception exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
 }
